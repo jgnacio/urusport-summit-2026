ce7e1d258ff434d521f1484b76f0f35d57144fa2
jgnacio <jgnaciogomez@gmail.com>
2025-11-08 07:53:05 -0300

    Comment out ChangeSection in Home component for future implementation

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <Header />
       <main>
         <HeroSection />
-      <ChangeSection />
+      {/* <ChangeSection /> */}
         <AboutSection />
         <EventsSection />
         <TeamSection />
